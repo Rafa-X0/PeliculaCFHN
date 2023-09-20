47af2b7b3d1ab6bb6c27e4c27c026c2305f659f8
Carlos Mangandí <me22001@esfe.agape.edu.sv>
2023-09-20 11:48:43 -0600

    Programación de las entidades

---
+++ PeliculasCFHN.ClienteBlazor/Models/Director.cs
@@ -0,0 +1,12 @@
+﻿namespace PeliculasCFHN.ClienteBlazor.Models
+{
+    public class Director
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string Nacionalidad { get; set; }
+        public string Imagen { get; set; }
+    }
+}
--- PeliculasCFHN.ClienteBlazor/Models/Pelicula.cs
+++ PeliculasCFHN.ClienteBlazor/Models/Pelicula.cs
@@ -2,5 +2,14 @@
 {
     public class Pelicula
     {
+        public int Id { get; set; }
+        public int IdGenero { get; set; }
+        public int IdDirector { get; set; }
+        public string Titulo { get; set; }
+        public string FechaEstreno { get; set; }
+        public string Actores { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+
     }
 }
